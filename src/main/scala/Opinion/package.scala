8d6d1cd63800be96236c9d604ed3972518c585c7
Sergio_Rodriguez <sergio.patino@correounivalle.edu.co>
2026-06-21 10:21:50 -0500

    Add agent-based polarization measure with interval-based pi_b

--- src/main/scala/Opinion/package.scala
+++ src/main/scala/Opinion/package.scala
@@ -13,6 +13,20 @@ package object Opinion {
       (i+1).toDouble/nags.toDouble )
   }
 
+  def midlyBelief(nags:Int): SpecificBelief = {
+    val middle = nags /2
+    Vector.tabulate(nags) ( ( i : Int ) =>
+      if ( i < middle)math.max (0.25-0.01*( middle-i-1),0)
+      else math.min(0.75-0.01*(middle-i),1))
+  }
+
+  def allExtremeBelief(nags:Int):SpecificBelief = {
+    val middle = nags /2
+    Vector.tabulate(nags) ( ( i :Int ) =>
+      if ( i < middle ) 0.0 else 1.0)
+  }
+
+
   def rho (alpha : Double , beta : Double ) : AgentsPolMeasure= {
     (agents: SpecificBelief , y:DistributionValues) => {
 
@@ -25,17 +39,35 @@ package object Opinion {
         bounds.zip(bounds.tail)
       }
 
-      def countInterval(entities:SpecificBelief,tuple:(Double,Double)): Int = {
-          entities.count(x => tuple._1 <= x && x < tuple._2 )
+      def countInterval(body:SpecificBelief,tuple:(Double,Double)): Int = {
+          body.count(x =>
+            if (tuple._2 == 1.0)
+              tuple._1 <= x && x <= tuple._2
+            else
+              tuple._1 <= x && x < tuple._2
+          )
       }
 
+      def pi_b(entities:SpecificBelief,distribution:DistributionValues):Vector[Double] = {
 
+        val bounds = buildBounds(distribution)
 
-      /*
-      def rhoAux(p: Double): Double = pi.zip(y).map { case (piI, yI) => math.pow(piI, alpha) * math.pow(math.abs(yI - p), beta)}.sum
+        val count = bounds.map(interval=>countInterval(entities,interval))
+
+        val totalAgents = entities.length.toDouble
+
+        count.map(_/totalAgents)
+
+      }
+
+      val pi = pi_b(agents,y)
+
+      def rhoAux(p: Double): Double = {
+        pi.zip(y).map { case (piI, yI) => math.pow(piI, alpha) * math.pow(math.abs(yI - p), beta)}.sum
+      }
       val pOptimo = min_p(rhoAux, 0.0, 1.0, 1e-6)
       rhoAux(pOptimo)
-      */
+
     }
   }
 
